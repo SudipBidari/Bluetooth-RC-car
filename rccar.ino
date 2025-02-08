9b64e7034c87c934ca9dead6f1b010a4fd3ef9aa
Sudip Bidari <134847589+SudipBidari@users.noreply.github.com>
2025-02-08 23:05:56 +0545

    Create rccar.ino

---
+++ rccar.ino
@@ -0,0 +1,70 @@
+// Define motor driver pins
+#define IN1 8  // Motor A input 1
+#define IN2 7  // Motor A input 2
+#define IN3 6  // Motor B input 1
+#define IN4 5  // Motor B input 2
+
+void setup() {
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+    Serial.begin(9600); // Start serial communication
+}
+
+void loop() {
+    if (Serial.available()) {
+        char command = Serial.read();
+        Serial.println(command);
+
+        // Control car movement
+        switch (command) {
+            case 'F': moveForward(); break;
+            case 'B': moveBackward(); break;
+            case 'L': turnLeft(); break;
+            case 'R': turnRight(); break;
+            case 'S': stopCar(); break;
+        }
+    }
+}
+
+// Function to move forward
+void moveForward() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+// Function to move backward
+void moveBackward() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
+
+// Function to turn left
+void turnLeft() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+// Function to turn right
+void turnRight() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
+
+// Function to stop the car
+void stopCar() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
